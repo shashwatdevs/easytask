e45feabdc5817b812286bd88f1aad3020fa88aba
shashwatdevs <shashwatgupta67@gmail.com>
2026-05-04 22:52:02 +0530

    user coponent added with some styles

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  standalone:true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+
+}
