fcac9d1ac0453c190d18f99152e2d7fba5a31126
shashwatdevs <shashwatgupta67@gmail.com>
2026-05-05 20:21:58 +0530

    @Input with required true

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -24,14 +24,14 @@ export class UserComponent {
   //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
   // }
 
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser(){
-    
+
   }
 }
