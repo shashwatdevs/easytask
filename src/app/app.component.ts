60253af5831273425ef00bfc0cee353b81652466
shashwatdevs <shashwatgupta67@gmail.com>
2026-05-05 21:37:36 +0530

    Tasks Comonent added | Configurable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,7 +17,13 @@ export class AppComponent {
 
   users = DUMMY_USERS;
 
+  selectedUserID =  "u1";
+
+  get selectedUser(){
+    return this.users.find((user)=>user.id === this.selectedUserID)!;
+  }
+
   onSelectUser(id:string){
-    console.log(id)
+    this.selectedUserID = id;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -16,7 +16,7 @@ export class UserComponent {
   @Input({required:true}) avatar!: string;
   @Input({required:true}) name!: string;
 
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   // select = output<string>(); 
 
