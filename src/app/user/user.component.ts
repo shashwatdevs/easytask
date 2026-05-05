3fa9a803ea748b691005d58a85d6f390164836a5
shashwatdevs <shashwatgupta67@gmail.com>
2026-05-05 21:10:42 +0530

    output signal instaed of @Output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal, input, Output, EventEmitter } from '@angular/core';
+import { Component, computed, Input, signal, input, Output, EventEmitter, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { required } from '@angular/forms/signals';
 
@@ -16,7 +16,9 @@ export class UserComponent {
   @Input({required:true}) avatar!: string;
   @Input({required:true}) name!: string;
 
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+
+  select = output<string>(); 
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
