bca138f2b91da59f59f85820b09808e4134810ff
shashwatdevs <shashwatgupta67@gmail.com>
2026-05-05 21:22:24 +0530

    go back to @Output Decorator approach

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -16,9 +16,9 @@ export class UserComponent {
   @Input({required:true}) avatar!: string;
   @Input({required:true}) name!: string;
 
-  // @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter();
 
-  select = output<string>(); 
+  // select = output<string>(); 
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
