d30f1551d3828a482a42e956ebb2ad6be851feac
shashwatdevs <shashwatgupta67@gmail.com>
2026-05-05 21:49:21 +0530

    Accepting object as Input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,19 +12,21 @@ import { required } from '@angular/forms/signals';
 })
 export class UserComponent {
 
-  @Input({required:true}) id!: string;
-  @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string;
+  // @Input({required:true}) id!: string;
+  // @Input({required:true}) avatar!: string;
+  // @Input({required:true}) name!: string;
+
+   @Input({required:true}) user!: {id:string, avatar:string, name: string};
 
   @Output() select = new EventEmitter<string>();
 
   // select = output<string>(); 
 
   get imagePath(){
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
