c3ec97243e82a40c5372e3f4720d1cdb92d47c53
shashwatdevs <shashwatgupta67@gmail.com>
2026-05-06 21:27:50 +0530

    Old style | NgIf | else (ngTemplate) | ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgForOf } from "../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
