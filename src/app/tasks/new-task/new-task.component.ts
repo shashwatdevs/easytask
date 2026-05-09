d4afb57ec7b535fa5497542817afaa4b73154c38
Shashwat Gupta <shashwatgupta67@gmail.com>
2026-05-09 21:40:47 +0530

    Directive | NgModel | FormModule | Two Way Binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
 
   @Output() cancle = new EventEmitter<void>();
+
+  enteredTitle = '';
 
   onCancleOrBack(){
     this.cancle.emit();
