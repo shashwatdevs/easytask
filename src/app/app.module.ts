55cc3ceea64d3680be138a3b104f061c7a98bae2
Shashwat Gupta <shashwatgupta67@gmail.com>
2026-05-10 12:46:57 +0530

    More Complex module | Nested Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,20 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { TasksComponent } from "./tasks/tasks.component";
 import { UserComponent } from "./user/user.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { DatePipe } from "@angular/common";
-import { FormsModule } from "@angular/forms";
 import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
     declarations: [
         AppComponent, 
         HeaderComponent, 
-        UserComponent, 
-        TasksComponent, 
-        TaskComponent, 
-        NewTaskComponent
+        UserComponent
     ],
     bootstrap: [AppComponent],
     // imports: [BrowserModule, DatePipe]
     // Not to iunclude DatePipe as already in BrowserModule
-    imports: [BrowserModule, FormsModule, SharedModule]
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {
 
